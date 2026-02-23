c4d6acabc8d327fc9a7699e41dcb1462a65d50bb
d0hyeon <ehgus6887@gmail.com>
2026-02-23 18:57:12 +0900

    feat: arrayIncludes 추가

--- src/shared/utils/types.ts
+++ src/shared/utils/types.ts
@@ -3,3 +3,7 @@ export type OmitPartial<T, Key extends keyof T> = Partial<Omit<T, Key>> & Pick<T
 export type PickPartial<T, Key extends keyof T> = Partial<Pick<T, Key>> & Omit<T, Key>;
 
 export type ValueOf<T> = T[keyof T];
+
+export function arrayIncludes<T>(value: T[], item: unknown): item is T {
+  return value.includes(item as T);
+}
