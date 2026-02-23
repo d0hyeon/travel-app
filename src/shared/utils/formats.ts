4081b75c1906a29cea05fd14ac3c4b7b1ba5b97d
d0hyeon <ehgus6887@gmail.com>
2026-02-23 18:57:04 +0900

    feat: format date 함수 추가

--- src/shared/utils/formats.ts
+++ src/shared/utils/formats.ts
@@ -2,3 +2,7 @@ export function formatDate(dateStr: string): string {
   const date = new Date(dateStr)
   return `${date.getMonth() + 1}/${date.getDate()}`
 }
+
+export function formatDateISO(value: string | number | Date) {
+  return new Date(value).toISOString().split('T')[0]
+}
