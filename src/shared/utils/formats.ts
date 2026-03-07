a94e086687ecf2135d7bb7426050f999eeac7435
d0hyeon <ehgus6887@gmail.com>
2026-03-08 02:17:50 +0900

    fix: UTC 적용

--- src/shared/utils/formats.ts
+++ src/shared/utils/formats.ts
@@ -1,10 +1,12 @@
+import { format } from "date-fns";
+
 export function formatDate(dateStr: string): string {
   const date = new Date(dateStr)
   return `${date.getMonth() + 1}/${date.getDate()}`
 }
 
 export function formatDateISO(value: string | number | Date) {
-  return new Date(value).toISOString().split('T')[0]
+  return format(value, 'yyyy-MM-dd')
 }
 
 
