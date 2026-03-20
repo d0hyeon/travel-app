bd721ffa1fcc3229113f8bdbe1e6f759d6ce00ff
d0hyeon <ehgus6887@gmail.com>
2026-03-20 17:14:34 +0900

    feat: lazy preload 기능 추가

--- src/shared/lib/react.ts
+++ src/shared/lib/react.ts
@@ -6,6 +6,11 @@ export function lazy<L extends Loader>(loader: L) {
   const Component = reactLazy(() => loader());
 
   return Object.assign(Component, {
-    preload: loader
+    preload: async () => {
+      const module = await loader();
+      if (module.preload instanceof Function) module.preload();
+      
+      return module;
+    }
   })
 }
