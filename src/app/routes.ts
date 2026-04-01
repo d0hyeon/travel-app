73d1b122e4fa6beb69d81d1ae057a387cbc3a710
d0hyeon <ehgus6887@gmail.com>
2026-04-01 18:28:30 +0900

    feat: 라우트 상수화

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,7 +1,12 @@
 import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
+export const AppRoute = {
+  메인: '/',
+  여행_상세: '/trip/:tripId'
+} as const;
+
 export default [
   index("trip/TripListPage.tsx"),
-  route("trip/:tripId", "trip/TripDetailPage.tsx"),
+  route(AppRoute.여행_상세, "trip/TripDetailPage.tsx"),
   route("*", "NotFound.tsx"),
 ] satisfies RouteConfig;
