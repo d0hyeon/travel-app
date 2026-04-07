e192a1b04c6ccb74867816d861fd8de15a560b12
d0hyeon <ehgus6887@gmail.com>
2026-04-07 16:25:48 +0900

    delete: 불필요한 로직 제거

--- src/shared/hooks/useIsMobile.ts
+++ src/shared/hooks/useIsMobile.ts
@@ -1,16 +1,6 @@
 import { useMediaQuery, useTheme } from '@mui/material'
-import { useMemo } from 'react';
 
 export function useIsMobile() {
-  const theme = useTheme();
-
-  const isMobile = useMemo(() => {
-    if (typeof window === 'undefined') return false; 
-    
-    const agent = window.navigator.userAgent;
-    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(agent);
-  }, [])
-  
-
-  return isMobile && useMediaQuery(theme.breakpoints.down('md'), { noSsr: true })
+  const theme = useTheme()
+  return useMediaQuery(theme.breakpoints.down('md'))
 }
