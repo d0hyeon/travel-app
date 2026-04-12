ec378a0f0c81b5aa2d995e9284762c49345040d2
d0hyeon <ehgus6887@gmail.com>
2026-04-12 16:49:52 +0900

    feat: UI 수정

--- src/features/trip/TripListPage.tsx
+++ src/features/trip/TripListPage.tsx
@@ -5,7 +5,7 @@ import {
   Stack,
   Typography
 } from '@mui/material'
-import { Link, useNavigate } from 'react-router'
+import { Link, PrefetchPageLinks, useNavigate } from 'react-router'
 import { AppRoute } from '~app/routes'
 import { useIsMobile } from '~shared/hooks/useIsMobile'
 import { useOverlay } from '~shared/hooks/useOverlay'
@@ -13,6 +13,8 @@ import { isOverseasByCoordinate } from '~shared/utils/geo'
 import { ListItem } from '../../shared/components/ListItem'
 import { TripFormDialog } from './TripFormDialog'
 import { useTrips } from './useTrips'
+import { BottomArea } from '~shared/components/BottomArea'
+import { BottomNavigation } from '~shared/components/BottomNavigation'
 
 export default function TripListPage() {
   const { data: trips, create } = useTrips();
@@ -20,60 +22,75 @@ export default function TripListPage() {
   const isMobile = useIsMobile();
   const navigate = useNavigate();
 
+  const openCreationPopup = () => {
+    overlay.open(({ isOpen, close }) => (
+      <TripFormDialog
+        open={isOpen}
+        onClose={close}
+        onSubmit={async (data) => {
+          const trip = await create({
+            ...data,
+            isOverseas: isOverseasByCoordinate(data.lat, data.lng),
+            exchangeRate: null,
+            exchangeRates: null
+          })
+          navigate(`/trip/${trip.id}`)
+        }}
+      />
+    ))
+  }
+
   return (
-    <Container maxWidth="sm" sx={{ py: 4 }}>
-      <Stack direction="row" justifyContent="space-between" alignItems="center" mb={3}>
-        <Typography variant="h6" >
-          내 여행
-        </Typography>
-        <Button
-          variant="contained"
-          onClick={() => {
-            if (isMobile) {
-              return navigate(AppRoute.여행_생성);
-            }
-
-            overlay.open(({ isOpen, close }) => (
-              <TripFormDialog
-                open={isOpen}
-                onClose={close}
-                onSubmit={async (data) => {
-                  const trip = await create({
-                    ...data,
-                    isOverseas: isOverseasByCoordinate(data.lat, data.lng),
-                    exchangeRate: null,
-                    exchangeRates: null
-                  })
-                  navigate(`/trip/${trip.id}`)
-                }}
-              />
-            ))
-          }}
-        >
-          새 여행
-        </Button>
-      </Stack>
-
-      {trips.length === 0 ? (
-        <Box textAlign="center" py={8}>
-          <Typography variant="body1" color="text.secondary">
-            아직 여행이 없어요. 새 여행을 만들어보세요!
+    <>
+      <Container maxWidth="sm" sx={{ py: 4, pb: 10 }}>
+        <Stack direction="row" justifyContent="space-between" alignItems="center" mb={3}>
+          <Typography variant="h6" >
+            내 여행
           </Typography>
-        </Box>
-      ) : (
-        <Stack spacing={2}>
-          {trips.map((trip, idx) => (
-            <Link key={trip.id} to={`/trip/${trip.id}`}>
-              <ListItem
-                leftAddon={<ListItem.Ordering>{idx + 1}</ListItem.Ordering>}
-              >
-                <ListItem.Title>{trip.name}</ListItem.Title>
-                <ListItem.Text>{trip.startDate} ~ {trip.endDate}</ListItem.Text>
-              </ListItem>
-            </Link>
-          ))}
+          {!isMobile && (
+            <Button
+              variant="contained"
+              onClick={() => openCreationPopup()}
+            >
+              새 여행
+            </Button>
+          )}
         </Stack>
+
+        {trips.length === 0 ? (
+          <Box textAlign="center" py={8}>
+            <Typography variant="body1" color="text.secondary">
+              아직 여행이 없어요. 새 여행을 만들어보세요!
+            </Typography>
+          </Box>
+        ) : (
+          <Stack spacing={2}>
+            {trips.map((trip, idx) => (
+              <Link key={trip.id} to={`/trip/${trip.id}`}>
+                <PrefetchPageLinks page={`/trip/${trip.id}`} />
+                <ListItem
+                  leftAddon={<ListItem.Ordering>{idx + 1}</ListItem.Ordering>}
+                >
+                  <ListItem.Title>{trip.name}</ListItem.Title>
+                  <ListItem.Text>{trip.startDate} ~ {trip.endDate}</ListItem.Text>
+                </ListItem>
+              </Link>
+            ))}
+          </Stack>
+        )}
+
+      </Container>
+      {isMobile && (
+        <BottomArea position="fixed" bottom={BottomNavigation.HEIGHT}>
+          <Button
+            variant="contained"
+            fullWidth
+            onClick={() => navigate(AppRoute.여행_생성)}
+          >
+            새 여행
+          </Button>
+        </BottomArea>
       )}
-    </Container>
+    </>
   )
 }
