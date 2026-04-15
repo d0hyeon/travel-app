b008a6a1912b4fcde8ae25c105da92d469b35dea
kdh <ehgus6887@gmail.com>
2026-04-15 21:25:23 +0900

    feat: Suspense 추가

--- src/features/statistics/StatisticsPage.tsx
+++ src/features/statistics/StatisticsPage.tsx
@@ -1,15 +1,16 @@
 import ReceiptLongIcon from '@mui/icons-material/ReceiptLong'
-import { Box, Stack, Tab, Tabs, Typography } from '@mui/material'
+import { Box, CircularProgress, LinearProgress, Stack, Tab, Tabs, Typography } from '@mui/material'
 import { useQueryParamState } from '~shared/hooks/urls/useQueryParamState'
 import { StatisticsCurrencySection } from './StatisticsCurrencySection'
 import { StatisticsExpenseSection } from './StatisticsExpenseSection'
 import { StatisticsOverviewSection } from './StatisticsOverviewSection'
 import { useStatisticsSummary } from './statistics-expense/useStatisticsSummary'
+import { Suspense } from 'react'
 
 type StatisticsTab = 'overview' | 'expense' | 'currency'
 
 export default function StatisticsPage() {
-  const summary = useStatisticsSummary()
+
   const [currentTab, setCurrentTab] = useQueryParamState<StatisticsTab>('stats-tab', {
     defaultValue: 'overview',
   })
@@ -20,15 +21,15 @@ export default function StatisticsPage() {
       <Box
         position="sticky"
         top={0}
-        px={2}
+
         py={1}
         pb={0}
         width="100%"
         bgcolor="background.paper"
         zIndex={10}
       >
-        <Stack gap={1.25}>
-          <Stack direction="row" alignItems="center" gap={1}>
+        <Stack gap={1.25} >
+          <Stack direction="row" alignItems="center" gap={1} px={2}>
             <ReceiptLongIcon color="primary" fontSize="small" />
             <Typography variant="h6" fontWeight={800}>
               통계
@@ -51,29 +52,43 @@ export default function StatisticsPage() {
         </Stack>
       </Box>
 
-      <Box px={2} py={2} flex={1} overflow="auto">
-        <Stack gap={3}>
-          {summary.travelSummaries.length === 0 ? (
-            <Box textAlign="center" py={8}>
-              <Typography variant="body1" color="text.secondary">
-                아직 기록된 지출이 없어요.
-              </Typography>
-            </Box>
-          ) : null}
+      <Suspense fallback={<Stack width="100%" alignItems="center" paddingY={10}><CircularProgress /></Stack>}>
+        <StatisticTabContent value={currentTab} />
+      </Suspense>
+    </Box>
+  )
+}
 
-          {summary.travelSummaries.length > 0 && currentTab === 'overview' ? (
-            <StatisticsOverviewSection summary={summary} />
-          ) : null}
+type Props = {
+  value: StatisticsTab;
+}
 
-          {summary.travelSummaries.length > 0 && currentTab === 'expense' ? (
-            <StatisticsExpenseSection summary={summary} />
-          ) : null}
+function StatisticTabContent({ value }: Props) {
+  const summary = useStatisticsSummary()
 
-          {summary.travelSummaries.length > 0 && currentTab === 'currency' ? (
-            <StatisticsCurrencySection summary={summary} />
-          ) : null}
-        </Stack>
-      </Box>
+  return (
+    <Box px={2} py={2} flex={1} overflow="auto">
+      <Stack gap={3}>
+        {summary.travelSummaries.length === 0 ? (
+          <Box textAlign="center" py={8}>
+            <Typography variant="body1" color="text.secondary">
+              아직 기록된 지출이 없어요.
+            </Typography>
+          </Box>
+        ) : null}
+
+        {summary.travelSummaries.length > 0 && value === 'overview' ? (
+          <StatisticsOverviewSection summary={summary} />
+        ) : null}
+
+        {summary.travelSummaries.length > 0 && value === 'expense' ? (
+          <StatisticsExpenseSection summary={summary} />
+        ) : null}
+
+        {summary.travelSummaries.length > 0 && value === 'currency' ? (
+          <StatisticsCurrencySection summary={summary} />
+        ) : null}
+      </Stack>
     </Box>
   )
 }
