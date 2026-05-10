c11bbb2e62dc63eafeef3e7432b4cf54276e94b7
kdh <ehgus6887@gmail.com>
2026-05-10 18:02:08 +0900

    fix: UI 버그 수정

--- src/shared/components/BottomArea.tsx
+++ src/shared/components/BottomArea.tsx
@@ -1,14 +1,14 @@
 import { Stack, type StackProps } from "@mui/material";
 
-export function BottomArea({ bottom, ...props }: StackProps) {
+export function BottomArea({ bottom, position = 'fixed', ...props }: StackProps) {
   return (
     <Stack
       direction="row"
       gap={1}
       padding={1}
-      position="fixed"
+      position={position}
       bottom={bottom ? `calc(${bottom}px + env(safe-area-inset-bottom))` : 0}
-      paddingBottom={bottom ? undefined : 'calc(8px + env(safe-area-inset-bottom))'}
+      paddingBottom={bottom == null && position === 'fixed' ? 'calc(8px + env(safe-area-inset-bottom))' : 1}
       width="100%"
       bgcolor="#fff"
       zIndex={10}
