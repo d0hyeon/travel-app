5bba50acb53386a10076cba2e71486c8188eb446
kdh <ehgus6887@gmail.com>
2026-05-26 14:47:24 +0900

    feat: 비율 변경

--- src/features/trip/trip-community-routes/CommunityRouteDetailOverlay.tsx
+++ src/features/trip/trip-community-routes/CommunityRouteDetailOverlay.tsx
@@ -37,7 +37,7 @@ export function useCommunityRouteDetailOverlay() {
 
 function CommunityRouteDetailSheet({ communityTrip, tripId, isOpen, onClose }: Props) {
   return (
-    <BottomSheet isOpen={isOpen} onClose={onClose} snapPoints={[0.85, 1]} defaultSnapIndex={1}>
+    <BottomSheet isOpen={isOpen} onClose={onClose} snapPoints={[0.85, 1]} defaultSnapIndex={0}>
       <BottomSheet.Body sx={{ p: 0 }}>
         <Suspense fallback={<DetailSkeleton />}>
           <DetailContent communityTrip={communityTrip} tripId={tripId} />
