ab5fa4dbe71f16f9416729bdb35c2393b8fb593d
kdh <ehgus6887@gmail.com>
2026-06-01 15:11:23 +0900

    refactor: 라우트 상수로 대체

--- src/features/trip/TripChatPage.tsx
+++ src/features/trip/TripChatPage.tsx
@@ -1,8 +1,9 @@
 import { useEffect } from 'react'
-import { useNavigate } from 'react-router'
-import { useTripChatOverlay } from './trip-chat/TripChatOverlay'
+import { generatePath, useNavigate } from 'react-router'
+import { useTripChatOverlay } from './trip-chat/useTripChatOverlay'
 import { useTripId } from './useTripId'
 import TripDetailPage from './TripDetailPage'
+import { AppRoute } from '~app/routes'
 
 export default function TripChatPage() {
   const tripId = useTripId()
@@ -10,9 +11,8 @@ export default function TripChatPage() {
   const { open } = useTripChatOverlay()
 
   useEffect(() => {
-    open(tripId)
-    navigate(`/trip/${tripId}`, { replace: true })
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    open(tripId);
+    navigate(generatePath(AppRoute.여행_상세, { tripId }), { replace: true })
   }, [])
 
   return <TripDetailPage />
