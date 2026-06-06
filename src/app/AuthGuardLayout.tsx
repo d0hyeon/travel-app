c51cad7d4f4b7e11cbeb474c4a7a2f54654ad010
kdh <ehgus6887@gmail.com>
2026-06-07 01:00:40 +0900

    fix: AuthGuard 추가

---
+++ src/app/AuthGuardLayout.tsx
@@ -0,0 +1,15 @@
+import { Outlet } from "react-router";
+import { AuthNavigate } from "~features/auth/AuthNavigate";
+import { useAuth } from "~features/auth/useAuth";
+
+export default function AuthGuardLayout() {
+  const { data: user } = useAuth({ required: false });
+
+  if (user == null) {
+    return <AuthNavigate />
+  }
+
+  return (
+    <Outlet />
+  )
+}
