7b9a4ee37da35c759a523affbf6d6196c9379b7a
kdh <ehgus6887@gmail.com>
2026-06-22 02:24:31 +0900

    feat: enabled 옵션 추가

--- src/shared/components/Map/kakao/useMapZoomLevel.ts
+++ src/shared/components/Map/kakao/useMapZoomLevel.ts
@@ -1,20 +1,26 @@
-import { useEffect, useState } from "react";
-import { KakaoMapContext, useMapContext } from "../MapContext";
+import { use, useEffect, useState } from "react";
+import { KakaoMapContext } from "../MapContext";
 
 
+type Options = {
+  enabled?: boolean;
+}
 
-export function useMapZoomLevel() {
-  const { map } = useMapContext(KakaoMapContext);
-  const [zoom, setZoom] = useState(map.getLevel());
+export function useMapZoomLevel({ enabled = true }: Options = {}) {
+  const context = use(KakaoMapContext);
+  const [zoom, setZoom] = useState(context?.map?.getLevel() ?? 8);
   
   useEffect(() => {
-    const zoomHandler = () => setZoom(map.getLevel());
-    kakao.maps.event.addListener(map, 'zoom_changed', zoomHandler);
+    if (!enabled || context?.map == null) return;
+    
+    setZoom(context.map.getLevel());
+    const zoomHandler = () => setZoom(context.map!.getLevel());
+    kakao.maps.event.addListener(context.map, 'zoom_changed', zoomHandler);
 
     return () => {
-      kakao.maps.event.removeListener(map, 'zoom_changed', zoomHandler);
+      kakao.maps.event.removeListener(context.map!, 'zoom_changed', zoomHandler);
     };
-  }, [])
+  }, [enabled, context?.map])
 
   return zoom;
 }
