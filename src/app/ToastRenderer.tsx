cf20c08745b61d44de3faa942de9fafa6f8ad524
kdh <ehgus6887@gmail.com>
2026-07-14 17:36:39 +0900

    feat: 토스트 기본 UI 스타일 적용

--- src/app/ToastRenderer.tsx
+++ src/app/ToastRenderer.tsx
@@ -1,25 +1,45 @@
+import CheckIcon from '@mui/icons-material/Check';
+import InfoIcon from '@mui/icons-material/Info';
+import ReportIcon from '@mui/icons-material/Report';
 import { GlobalStyles } from "@mui/material";
 import { Toaster } from "sonner";
+import { useIsMobile } from "~shared/hooks/env/useIsMobile";
+
 
 export function ToastRenderer() {
+  const isMobile = useIsMobile();
+
   return (
     <>
       <GlobalStyles
         styles={theme => ({
           '.toast-container': {
-            justifyContent: 'space-between',
             borderRadius: '16px !important;',
             boxShadow: '3px 5px 18px rgba(0, 0, 0, 0.1.5) !important;',
             paddingBlock: '12px !important;',
+            fontFamily: theme.typography.fontFamily,
             [theme.breakpoints.up('md')]: {
               paddingBlock: '8px !important;',
             }
+          },
+          '.warning-message .toast-icon': {
+            color: theme.palette.warning.main
           }
         })}
       />
       <Toaster
         toastOptions={{
-          classNames: { toast: 'toast-container' },
+          classNames: {
+            toast: 'toast-container',
+            warning: 'warning-message',
+            icon: 'toast-icon'
+          },
+        }}
+        position={isMobile ? 'top-center' : 'top-right'}
+        icons={{
+          info: <InfoIcon fontSize="small" />,
+          success: <CheckIcon fontSize="small" color="success" />,
+          error: <ReportIcon fontSize="small" color="error" />
         }}
       />
     </>
