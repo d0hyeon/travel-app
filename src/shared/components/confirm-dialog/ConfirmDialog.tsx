6d4ee710fcbbecc8da712f8f98ef23a95f8b45b8
kdh <ehgus6887@gmail.com>
2026-08-15 04:23:51 +0900

    feat(CofirmDialog): 타이틀 스타일 변경

--- src/shared/components/confirm-dialog/ConfirmDialog.tsx
+++ src/shared/components/confirm-dialog/ConfirmDialog.tsx
@@ -39,7 +39,7 @@ export function ConfirmDialog({ title, description, isOpen, confirmLabel = 'í™•ì
       aria-labelledby="custom-dialog-title"
       aria-describedby="custom-dialog-description"
     >
-      <DialogTitle id="custom-dialog-title" sx={{ whiteSpace: 'pre-wrap' }}>
+      <DialogTitle id="custom-dialog-title" sx={{ whiteSpace: 'pre-wrap', fontSize: 14 }}>
         {title}
       </DialogTitle>
       {description && (
